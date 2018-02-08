247b647d8815ddb216776a149978a0465006c904
HexBotOfficial <36220797+HexBotOfficial@users.noreply.github.com>
2018-02-08 09:23:37 +0800

    Update Arm_quality_test.ino

--- Arm/examples/Arm_quality_test/Arm_quality_test.ino
+++ Arm/examples/Arm_quality_test/Arm_quality_test.ino
@@ -22,21 +22,21 @@ void setup() {
 
 void loop() {
 
- //姿态1
+    //姿态1
     MyArm.move_to_position(100.0, 159,220,500);
-    //MyArm.turn_steer_345_to_positon((word)2300,2600,2700,500);
+    MyArm.turn_steer_345_to_positon((word)2300,2600,2700,500);
     delay(510);
     //姿态2
     MyArm.move_to_position(-100.0, 139,220,1700);
-   // MyArm.turn_steer_345_to_positon((word)1700,1400,1300,700);
+    MyArm.turn_steer_345_to_positon((word)1700,1400,1300,700);
     delay(1710);
     //姿态3
     MyArm.move_to_position(-100.0, 70, 160,500);
-    //MyArm.turn_steer_345_to_positon((word)2500,2400,2700,500);
+    MyArm.turn_steer_345_to_positon((word)2500,2400,2700,500);
     delay(510);
     //姿态4
     MyArm.move_to_position(100.0, 70, 160,2000);
-   // MyArm.turn_steer_345_to_positon((word)2047,2047,2047,700);    
+    MyArm.turn_steer_345_to_positon((word)2047,2047,2047,700);    
     delay(2010);
 }
 
